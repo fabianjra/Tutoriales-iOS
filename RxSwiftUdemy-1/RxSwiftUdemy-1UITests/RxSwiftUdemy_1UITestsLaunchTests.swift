bd9b7863cd1f32783df9ecaf04c4aad9d85d5438
Fabian Rodriguez <fabianjra@hotnail.com>
2022-10-25 14:47:22 -0600

    Se agrega el proyecto de RxSwift de Udemy (numero 1).

---
+++ RxSwiftUdemy-1/RxSwiftUdemy-1UITests/RxSwiftUdemy_1UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  RxSwiftUdemy_1UITestsLaunchTests.swift
+//  RxSwiftUdemy-1UITests
+//
+//  Created by Fabian Josue Rodriguez Alvarez on 25/10/22.
+//
+
+import XCTest
+
+final class RxSwiftUdemy_1UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
