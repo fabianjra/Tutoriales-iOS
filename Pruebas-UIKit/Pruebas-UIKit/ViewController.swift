ced63032278cc23d16ff97f9513445383be59ff9
Fabian Rodriguez <fabianjra@hotnail.com>
2022-11-03 13:44:30 -0600

    Pruebas con constraints: https://youtu.be/Xe6S_c5ApIc

--- Pruebas-UIKit/Pruebas-UIKit/ViewController.swift
+++ Pruebas-UIKit/Pruebas-UIKit/ViewController.swift
@@ -7,11 +7,13 @@
 
 import UIKit
 
-//NOTA: Los IBOulets vienen con un Force Unwrapped, porque lo primero que se carga es el ViewDidLoad, asi que se garantiza que esos Oulets tengan valores.
-
 //CICLO DE VIDA DE UN ViewController:
 //Se hereda a UIViewController, por eso al llamar a "super." dentro de los metodos, se llama la logica herededada del UIViewController.
 class ViewController: UIViewController {
+    
+    //NOTA: Los IBOulets vienen con un Force Unwrapped, porque lo primero que se carga es el ViewDidLoad, asi que se garantiza que esos Oulets tengan valores.
+    @IBOutlet weak var constraintCenterLabel: NSLayoutConstraint!
+    @IBOutlet weak var contraintTopLabel: NSLayoutConstraint! //Segundo Constraint, desactivado por defecto. Utilizado para subir el label al Top.
     
     //Estos llamados a las vistas son realizadas automaticamente por el sistema.
     //El ContentView es la vista principal que cotiene el Storyboard.
@@ -20,7 +22,9 @@ class ViewController: UIViewController {
     //Its called only for the first time.
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //Para practica de constraints por codigo.
+        createButton(view)
     }
 
     //2: Called before "ContentView" is added to app's view hierarchy.
@@ -66,6 +70,81 @@ class ViewController: UIViewController {
     //Maybe for an animation.
     override func viewDidLayoutSubviews() {
         
+    }
+    
+    //MARK: - CONSTRAINTS:
+    
+    private func createButton(_ view: UIView) {
+        
+        let button = UIButton(type: .system)
+        button.setTitle("Psuh me", for: .normal)
+        button.addTarget(self, action: #selector(bottonPushed), for: .touchUpInside)
+        
+        //Se crea el boton y en los constraints, se indica que se ajuste al bottom de la pantalla
+        view.addSubview(button)
+        
+        //cuando los constraint se crean se deben activar.
+        let leftConstraint = button.leftAnchor.constraint(equalTo: view.leftAnchor) //Esto crea un constraint, pero se debe activar
+        leftConstraint.isActive = true
+        
+        //Utilizar el idioma correcto: Leading o Trailig
+        //let rightConstraint = button.rightAnchor.constraint(equalTo: view.rightAnchor)
+        
+        //Se utiliza Trailing porque es derecho.
+        let rightConstraint = button.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        rightConstraint.isActive = true
+        
+        //Este es un ejemplo de un constraint creado sin tomar en cuenta el SafeArea:
+        //let bottomConstraint = button.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        
+        //Mismo constraint creado tomando en cuenta el SafeArea, se agrega: "safeAreaLayoutGuide"
+        let bottomConstraint = button.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        bottomConstraint.isActive = true
+
+        //Despues de crear los constraints por codigo, hay 1 cosa mas que hacer
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        //En caso de que se quiera agregar un Width especifico.
+        //Al agregarlo, muestra el error:
+        /*
+         Will attempt to recover by breaking constraint
+         <NSLayoutConstraint:0x6000026f34d0 UIButton:0x143f071f0'Psuh me'.width == 100   (active)>
+         */
+        //button.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        
+        //Una forma facil de identificar el Constraint que está fallando, es agregar un identificador, para ver en el LOG cual es el incorrecto.
+        let widthConstraint = button.widthAnchor.constraint(equalToConstant: 100)
+        widthConstraint.identifier = "bottom button Width"
+        widthConstraint.isActive = true
+    }
+    
+    @objc func bottonPushed(_ button: UIButton) {
+        
+        adjustViewPosition()
+    }
+    
+    private func adjustViewPosition() {
+        //NOTA: Cuando se utiliza AutoLayout en los constraint, no se debe cambiar el size o position, con CGRect o CGPoint.
+        
+        //Se debe acceder al constraint que ya tiene asignado en el Storyboard, para subir la vista al Top Screen, que actualmente está centrado.
+        
+        //Esta manera no es la correcta, por lo que una forma conveniente de hacerlo, es tener 2 constraint y usar uno u otro, dependiendo de la necesidad.
+        //constraintCenterLabel.constant = -view.frame.size.height / 2 + 100 //Se sube la vista, ajustando la constante del Constraint.
+        
+        //Al agregar un segundo constraint, se ve una casilla que dice "installed" al seleccionar ese contraint. Eso indica si el Constraint está activo o no. (en las opciones del storyboard).
+        //Lo que se debe hacer, es desactivar el constraint que centra el label y activar el del TOP (Segundo constraint agregado).
+        //constraintCenterLabel.isActive = false
+        //contraintTopLabel.isActive = true
+        
+        //validacion para hacerlo dinamico.
+        if constraintCenterLabel.isActive {
+            
+            constraintCenterLabel.isActive = false
+            contraintTopLabel.isActive = true
+        } else {
+            constraintCenterLabel.isActive = true
+            contraintTopLabel.isActive = false
+        }
     }
 }
 
