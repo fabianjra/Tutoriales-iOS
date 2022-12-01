602264b7b1c9b0e0bbbe352793abfab8bbb82533
Fabian Rodriguez <fabianjra@hotnail.com>
2022-12-01 13:43:21 -0500

    RxSwift: uso de DisposeBag y Publish Subject

--- RxSwift/RxSwift-Udemy-1/RxSwift-Udemy-1/RxSwift.playground/Contents.swift
+++ RxSwift/RxSwift-Udemy-1/RxSwift-Udemy-1/RxSwift.playground/Contents.swift
@@ -1,6 +1,10 @@
 import UIKit
 import RxSwift
 
+//====================================================================================================================//
+//        BASICS      //
+//====================================================================================================================//
+
 _ = Observable.from([1, 2, 3, 4, 5])
 
 //Just contiene un solo objeto.
@@ -23,15 +27,16 @@ private func Observable3(){
         }
         
         print(element)
+        //[1, 2, 3]
     }
     
 }
-Observable3()
+//Observable3()
 
 private func Observable4(){
     
     //Se usa para un array especificamente.
-    //Of es para la funcion de un array, pero from es para crear el array de forma individual.
+    //FROM: Convierte los elementos del array a items individuales. A diferencia de "of" que hace un observable de todo el array como un unico elemento.
     let observable4 = Observable.from([1, 2, 3, 4, 5])
     
     //Permite obtener el dato del obejto al que se esta observando.
@@ -64,5 +69,123 @@ private func Observable4(){
          Vacio
          */
     }
+    
+    //"onNext": La forma mas facil o correcta de acceder al elemento especificamente.
+    let subscription4 = observable4.subscribe(onNext: { element in
+        print("onNext element: ", element)
+    })
+    
+    //"DISPOSE": Cuando se usa un subscriber, él se va a quedar escuchado u observando. Se tiene que eliminar con dispose.
+    //Si no se hace dispose, podria provocarse un memory leak.
+    subscription4.dispose()
 }
 //Observable4()
+
+
+//====================================================================================================================//
+//        DISPOSE      //
+//====================================================================================================================//
+
+/// Como utilizar DisposeBag correctamente para un Subscribe.
+private func disposeFunction1(){
+    
+    //Se crea el disposeBag para poder hacerle Dispose al observable.
+    let disposeBag = DisposeBag()
+    
+    //Un subscribe debe ser siempre disposable, por medio de un "DisposeBag".
+    Observable.of("A", "B", "C")
+        .subscribe{
+            print($0)
+        }.disposed(by: disposeBag) //Se utiliza el diposeBag para eliminarlo.
+    
+}//End function
+//disposeFunction1()
+
+
+//====================================================================================================================//
+//        "Create" Observable      //
+//====================================================================================================================//
+
+/// Una segunda manera de crear un Observable.
+/// Crear el Observable con "create", permite describir cada paso que se ejecuta.
+private func observableExample2(){
+    
+    let disposeBag = DisposeBag()
+    
+    //Esta manera de instanciar, brinda un oberver.
+    Observable<String>.create { observer in
+        
+        observer.onNext("Fabian, unico elemento como string")
+        observer.onCompleted()
+        observer.onNext("Texto string despues de onCompleted()") //No se ejecuta, porque antes se llama a "onCompleted()"
+        
+        //"Create": Obliga a retornar un disposable.
+        return Disposables.create()
+    }
+    //Se puede crear el subscriber desde el mismo Observable
+    .subscribe(onNext: { element in
+        
+        print("1: Se imprimie el elemento de onNext:", element)
+        
+    }, onError: { error in
+        
+        print("2: Se ejecuto onError: ", error)
+        
+    }, onCompleted: {
+        
+        print("3: Ejecucion de onCompleted")
+        
+    }, onDisposed: {
+        
+        print("4: Ejecucion de onDisposed")
+        
+    })
+    .disposed(by: disposeBag)
+    
+}//End function
+//observableExample2()
+
+//====================================================================================================================//
+//        SUBJECTS      //
+//====================================================================================================================//
+
+/*
+ They are "Observables" as well the "Observers".
+ Tipos de Subjects:
+ 
+ 1: Publish subjects
+ 2: Replay subject
+ 3: BehaviorRelay
+ 4: Variable
+*/
+
+private func publishSubject_1(){
+    let disposeBag = DisposeBag()
+    
+    let subject = PublishSubject<String>()
+    
+    //Este evento no se va a mostrar, porque aun no se ha realizado la subscripcion.
+    subject.onNext("onNext 1 - Publish Subject")
+    
+    //"Subscribe": Es el encargado de realizar algo, ya que se subscribe y está a la escucha del PublishSubject.
+    //Sin subsribe, no pasa nada.
+    //El subsribe se realiza sobre el mismo objeto del Publish.
+    subject.subscribe{ event in
+        
+        print("Esta es la subscripcion: ", event)
+    }
+    
+    //Estos elementos si se muestran en el print, porque ya existe una subscripcion.
+    subject.onNext("onNext 2: Despues de que el subscriber se crea")
+    subject.onNext("onNext 3: Tercer evento.")
+    
+    subject.onCompleted() //se da por completado el Observer, pero se muestra como un evento más.
+    
+    subject.onNext("onNext 4: despues de completed") //Este no se ejecuta porque está despues del onCompleted().
+    
+    subject.dispose() //Se elimina el publish Subject
+    
+    subject.onNext("onNext 5: ultimo") //Este no se ejecuta porque está despues del dispose.
+    
+}//End function
+publishSubject_1()
