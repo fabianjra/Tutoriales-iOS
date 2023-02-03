f08bcffdba4cabde053f0da4c478eb8601d985a7
Fabian <fabianjra@hotmail.com>
2023-02-03 00:30:34 -0600

    Se remueven advertencias

--- MVVM-DesignPattern-Udemy/Apps/GoodNews/GoodNews/Controllers/NewsListTableViewController.swift
+++ MVVM-DesignPattern-Udemy/Apps/GoodNews/GoodNews/Controllers/NewsListTableViewController.swift
@@ -22,8 +22,8 @@ class NewsListTableViewController: UITableViewController {
     
     private func loadData() {
         
-        let errorExample = "https://newsapi.org/v2/top-headlines"
-        let urlCountryUS = "https://newsapi.org/v2/top-headlines?country=us&apiKey=427fadf7ee1c4d9185b142d260add28e"
+        _ = "https://newsapi.org/v2/top-headlines"
+        _ = "https://newsapi.org/v2/top-headlines?country=us&apiKey=427fadf7ee1c4d9185b142d260add28e"
         let urlBBC = "https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=427fadf7ee1c4d9185b142d260add28e"
         
         let url = URL(string: urlBBC)! //Force because is hard coded
