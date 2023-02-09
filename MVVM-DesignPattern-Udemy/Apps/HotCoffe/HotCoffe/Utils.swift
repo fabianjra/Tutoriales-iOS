c10191472f36498a4cfd33e79a362738b7ada96d
Fabian <fabianjra@hotmail.com>
2023-02-09 01:55:25 -0600

    Add comments section for functions.

--- MVVM-DesignPattern-Udemy/Apps/HotCoffe/HotCoffe/Utils.swift
+++ MVVM-DesignPattern-Udemy/Apps/HotCoffe/HotCoffe/Utils.swift
@@ -7,17 +7,66 @@
 
 enum Log {
     
-    /// Shows an Catch error message on console, and an optional string message.
-    /// - Parameters:
-    ///    - message: Optional string message to add to the print.
-    ///    - err: The catch error message from the try.
-    static func WriteCatchExeption(_ message: String? = nil, file: String = #file, function: String = #function, line: Int = #line, err: Error) {
-        print("Error in catch - \(message ?? ""), called by: \(file.components(separatedBy: "/").last ?? file) - \(function), at line: \(line). Description: ", err)
+    /**
+     Shows an Catch error message on console, and an optional string message.
+     
+     **Notes:**
+     - This function takes the file name where the error is presented, the function name who call it and the line number where the error is presented.
+     - By default **file**, **function** and **line** are setted internally, so you can ignore these parameters.
+     
+     **Example:**
+     ```swift
+     do{
+        let article = try JSONDecoder().decode(T.self, from: data)
+     
+         if let article = article {
+            completion(article)
+         }
+     }catch{
+        Log.WriteCatchExeption(err: error)
+     }
+     ```
+    
+     - Parameters:
+        - message:Optional string message to add to the print.
+        - error: The catch error message from the try.
+     
+     - Returns: Void
+     
+     - Warning: N/A
+     
+     - Throws: N/A
+     
+     - Authors: Fabian Rodriguez
+     
+     - Version: 1.0
+     
+     - Date: February 2023
+     */
+    static func WriteCatchExeption(_ message: String? = nil, file: String = #file, function: String = #function, line: Int = #line, error: Error) {
+        print("Error in catch - \(message ?? ""), called by: \(file.components(separatedBy: "/").last ?? file) - \(function), at line: \(line). Description: ", error)
     }
     
-    /// Shows a string message on console.
-    /// - Parameters:
-    ///    - message: Menssage to write on console..
+    /**
+     Shows a message on console.
+     
+     **Notes:**
+     - Writes a simple string message on console.
+     
+     **Example:**
+     ```swift
+     Log.WriteMessage("This is a string message.")
+     ```
+    
+     - Parameters:
+        - message:String message to add to the print.
+     
+     - Authors: Fabian Rodriguez
+     
+     - Version: 1.0
+     
+     - Date: February 2023
+     */
     static func WriteMessage(_ message: String) {
         print(message)
     }
