40394b9606578b2dfbbf36474d08db70988d474b
Fabian <fabianjra@hotmail.com>
2023-02-10 15:20:06 -0600

    Udemy: MVVM Design Pattern Using Swift in iOS: -- 24. Populating Cup Sizes Using View Model || 25. Adding Checkmark for Selected Cells -- Se crea el control "SegmentedControl" por medio de código, se agregan los contravino y se llena con los datos. || Logica de selección de celda para los CoffeType.

--- MVVM-DesignPattern-Udemy/Apps/HotCoffe/HotCoffe/Controllers/AddOrderViewController.swift
+++ MVVM-DesignPattern-Udemy/Apps/HotCoffe/HotCoffe/Controllers/AddOrderViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class AddOrderViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
+    private var coffeSizeSegmentedControl: UISegmentedControl!
     
     //This is for controling all the things are going to present in the UI.
     private let addOrderViewModel = AddOrderViewModel()
@@ -17,8 +18,24 @@ class AddOrderViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupView()
+    }
+    
+    private func setupView(){
         tableView.delegate = self
         tableView.dataSource = self
+        
+        self.coffeSizeSegmentedControl = UISegmentedControl(items: addOrderViewModel.sizes)
+        self.coffeSizeSegmentedControl.selectedSegmentIndex = 0
+        
+        self.coffeSizeSegmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(coffeSizeSegmentedControl)
+        
+        self.coffeSizeSegmentedControl.topAnchor.constraint(equalTo: tableView.bottomAnchor, constant: 10).isActive = true
+        self.coffeSizeSegmentedControl.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        //It has intrinsic content size
+        //self.coffeSizeSegmentedControl.heightAnchor.constraint(equalToConstant: 40).isActive = true
     }
 }
 
@@ -26,6 +43,17 @@ extension AddOrderViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.addOrderViewModel.types.count
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        //Add de selection icon to the right of cell.
+        tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+    }
+    
+    //Allows to deselect the previus row.
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        tableView.cellForRow(at: indexPath)?.accessoryType = .none
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
