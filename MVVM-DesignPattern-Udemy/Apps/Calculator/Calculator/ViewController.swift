3d2f73dbb3b446c1a0210928855f61fb2cdf0b73
Fabian <fabianjra@hotmail.com>
2023-02-24 13:08:16 -0600

    Udemy: MVVM Design Pattern Using Swift in iOS: -- 58. Setting Up Your Project -- Creacion del proyecto base para TEST

---
+++ MVVM-DesignPattern-Udemy/Apps/Calculator/Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Fabian Josue Rodriguez Alvarez on 24/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
