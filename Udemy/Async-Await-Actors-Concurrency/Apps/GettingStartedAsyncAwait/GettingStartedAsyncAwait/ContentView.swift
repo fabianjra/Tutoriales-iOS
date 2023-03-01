d2c54ad689a06815fa7e2e7a536e13aab37dc16f
Fabian <fabianjra@hotmail.com>
2023-02-28 23:19:30 -0600

    Udemy: Async/Await and Actors - Concurrency in Swift: -- 11. Tour of the Starter Project -- Se agrega el proyecto base creado desde el perfil de Fabian.

---
+++ Udemy/Async-Await-Actors-Concurrency/Apps/GettingStartedAsyncAwait/GettingStartedAsyncAwait/ContentView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  GettingStartedAsyncAwait
+//
+//  Created by Fabian Josue Rodriguez Alvarez on 28/2/23.
+//
+
+import SwiftUI
+
+struct CurrentDate: Decodable, Identifiable {
+    let id = UUID()
+    let date: String
+    
+    private enum CodingKeys: String, CodingKey {
+        case date = "date"
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView {
+            List(1...10, id: \.self) { index in
+                Text("\(index)")
+            }.listStyle(.plain)
+            
+                .navigationTitle("Dates")
+                .navigationBarItems(trailing: Button(action: {
+                    //Button Action
+                }, label: {
+                    Image(systemName: "arrow.clockwise.circle")
+                }))
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Udemy/Async-Await-Actors-Concurrency/Apps/GettingStartedAsyncAwait/GettingStartedAsyncAwait/GettingStartedAsyncAwaitApp.swift
@@ -0,0 +1,17 @@
+//
+//  GettingStartedAsyncAwaitApp.swift
+//  GettingStartedAsyncAwait
+//
+//  Created by Fabian Josue Rodriguez Alvarez on 28/2/23.
+//
+
+import SwiftUI
+
+@main
+struct GettingStartedAsyncAwaitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
