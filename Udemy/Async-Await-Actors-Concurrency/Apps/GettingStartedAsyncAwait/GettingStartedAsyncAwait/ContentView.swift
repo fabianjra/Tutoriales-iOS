897dfd06433115bca3feac8dc6fd9d36ab94e982
Fabian <fabianjra@hotmail.com>
2023-03-01 00:15:32 -0600

    Udemy: Async/Await and Actors - Concurrency in Swift: -- 12. Implementing getDate Async Function -- Se crea la función para obtener el Json y se pasa al modelo.

--- Udemy/Async-Await-Actors-Concurrency/Apps/GettingStartedAsyncAwait/GettingStartedAsyncAwait/ContentView.swift
+++ Udemy/Async-Await-Actors-Concurrency/Apps/GettingStartedAsyncAwait/GettingStartedAsyncAwait/ContentView.swift
@@ -17,6 +17,20 @@ struct CurrentDate: Decodable, Identifiable {
 }
 
 struct ContentView: View {
+    
+    private func getDate() async throws -> CurrentDate? {
+       let urlString = "https://ember-sparkly-rule.glitch.me/current-date"
+       
+       guard let url = URL(string: urlString) else {
+           fatalError("El URL es incorrecto")
+       }
+        
+        let (data, _) = try await URLSession.shared.data(from: url)
+        
+        //If the app crashes: The try will return nil, because it's optional.
+        return try? JSONDecoder().decode(CurrentDate.self, from: data)
+   }
+    
     var body: some View {
         NavigationView {
             List(1...10, id: \.self) { index in
