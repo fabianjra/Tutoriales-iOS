2e54a00916efa3e62b9291656c7741cfb7bad0dd
Fabian <fabianjra@hotmail.com>
2023-04-04 15:29:02 -0600

    Udemy: Async/Await and Actors - Concurrency in Swift: -- 45. Module Introduction -- Se crea el proyecto base para trabajar en el modulo sección 9

---
+++ Udemy/Async-Await-Actors-Concurrency/Apps/RandomQuoteAndImages/RandomQuoteAndImages/RandomQuoteAndImagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomQuoteAndImagesApp.swift
+//  RandomQuoteAndImages
+//
+//  Created by Fabian Josue Rodriguez Alvarez on 4/4/23.
+//
+
+import SwiftUI
+
+@main
+struct RandomQuoteAndImagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
