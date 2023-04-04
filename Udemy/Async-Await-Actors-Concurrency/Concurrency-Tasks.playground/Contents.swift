f764a15b69d161b764a5e3d3c15087f3dc0bba1b
Fabian <fabianjra@hotmail.com>
2023-04-03 23:33:44 -0600

    Udemy: Async/Await and Actors - Concurrency in Swift: -- 40. Cancelling a Task -- Se agrega la funcion try para saber si un llamado al API provoca un error y se captura dentro del loop

--- Udemy/Async-Await-Actors-Concurrency/Concurrency-Tasks.playground/Contents.swift
+++ Udemy/Async-Await-Actors-Concurrency/Concurrency-Tasks.playground/Contents.swift
@@ -1,5 +1,11 @@
 import UIKit
 
+enum NetworkError: Error {
+    case badUrl
+    case decodingError
+    case invalidId
+}
+
 struct CreditScore: Decodable {
     let score: Int
 }
@@ -58,15 +64,56 @@ Task {
     print(apr)
 }
 
+//*******************************************************************************//
+
+print("LOOP CALL STARTS:")
+
+func getAPR_ConError(userId: Int) async throws -> Double {
+    
+    if userId % 2 == 0 {
+        
+        throw NetworkError.invalidId
+    }
+    
+    guard let equifaxUrl = Constants.Urls.equifax(userId: userId),
+          let experianUrl = Constants.Urls.experian(userId: userId) else {
+        throw NetworkError.badUrl
+    }
+    
+    async let (equifaxData, _) = URLSession.shared.data(from: equifaxUrl)
+    async let (experianData, _) = URLSession.shared.data(from: experianUrl)
+
+    let equifaxCreditScore = try? JSONDecoder().decode(CreditScore.self, from: try await equifaxData)
+    let experianCreditScore = try? JSONDecoder().decode(CreditScore.self, from: try await experianData)
+    
+    guard let equifaxCreditScore = equifaxCreditScore,
+          let experianCreditScore = experianCreditScore else {
+        throw NetworkError.decodingError
+    }
+    
+    return calculateAPR(creditScores: [equifaxCreditScore, experianCreditScore])
+}
 
 //Loop:
-
 let ids = [1, 2, 3, 4, 5]
+var invalidIds: [Int] = []
 
 Task {
     for id in ids {
-        let apr = try await getAPR(userId: id)
         
-        print(apr)
+        do {
+            //"CheckCancellation": Permite saber si hay un throw y se produce un error en la funcion.
+            try Task.checkCancellation()
+            
+            let apr = try await getAPR_ConError(userId: id)
+            
+            print(apr)
+        } catch {
+            print(error)
+            invalidIds.append(id) //almacena los Id con error para saber cuales estan fallando
+        }
     }
+    
+    //Array con los que contiene error:
+    print(invalidIds)
 }
