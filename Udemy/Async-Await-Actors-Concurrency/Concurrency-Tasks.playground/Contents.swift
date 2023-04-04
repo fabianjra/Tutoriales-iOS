498214e5c7af30355e47fbba9288acbfc63e273e
Fabian <fabianjra@hotmail.com>
2023-04-03 22:55:34 -0600

    Udemy: Async/Await and Actors - Concurrency in Swift: -- 38. Async-let Tasks -- Async let para hacer llamados async

--- Udemy/Async-Await-Actors-Concurrency/Concurrency-Tasks.playground/Contents.swift
+++ Udemy/Async-Await-Actors-Concurrency/Concurrency-Tasks.playground/Contents.swift
@@ -17,6 +17,13 @@ struct Constants {
     }
 }
 
+func calculateAPR(creditScores: [CreditScore]) -> Double {
+    let sum = creditScores.reduce(0) { next, credit in
+        return next + credit.score
+    }
+    return Double((sum/creditScores.count)/100)
+}
+
 func getAPR(userId: Int) async throws -> Double {
     
     guard let equifaxUrl = Constants.Urls.equifax(userId: userId),
@@ -24,14 +31,28 @@ func getAPR(userId: Int) async throws -> Double {
         throw NetworkError.badUrl
     }
     
-    do{
-        let (equifaxData, _) = try await URLSession.shared.data(from: equifaxUrl)
-        let (experianData, _) = try await URLSession.shared.data(from: experianUrl)
-        
-    }catch {
-        
+    //Cuando se use "async let", no es necesario agregar "try await" al URL Session.
+    async let (equifaxData, _) = URLSession.shared.data(from: equifaxUrl)
+    async let (experianData, _) = URLSession.shared.data(from: experianUrl)
+    
+    //Se usa await en data, porque la variable es async.
+    //A demas se usa try, porque "URLSession.shared.data" es throws
+    //En este punto es en donde la aplicacion va a esperar el llamado, porque aqui es donde tiene el await.
+    //La idea de hacerlo de esta forma, es que cualquiera de las 2 pueden terminar primero porque se hacen independientes.
+    let equifaxCreditScore = try? JSONDecoder().decode(CreditScore.self, from: try await equifaxData)
+    let experianCreditScore = try? JSONDecoder().decode(CreditScore.self, from: try await experianData)
+    
+    //Wunwrap las respuestas porque usan try opcional:
+    guard let equifaxCreditScore = equifaxCreditScore,
+          let experianCreditScore = experianCreditScore else {
+        throw NetworkError.decodingError
     }
     
+    return calculateAPR(creditScores: [equifaxCreditScore, experianCreditScore])
+}
+
+Task {
+   let apr = try await getAPR(userId: 1)
     
-    return 0.0
+    print(apr)
 }
