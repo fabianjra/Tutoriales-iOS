e9e5b2adefd505851d8500d3eb585c120ceac8b4
Fabian <fabianjra@hotmail.com>
2023-04-04 00:01:44 -0600

    Udemy: Async/Await and Actors - Concurrency in Swift: -- 42. Unstructured Tasks -- PT1: Se agrega el proyecto base para el ejemplo.

---
+++ Udemy/Async-Await-Actors-Concurrency/Apps/UnstructuredTasks/UnstructuredTasks/ContentView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  UnstructuredTasks
+//
+//  Created by Fabian Josue Rodriguez Alvarez on 3/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    private func getData() async {
+        //get the data
+    }
+    
+    var body: some View {
+        VStack {
+            
+            Button {
+                
+            } label: {
+                Text("Get data")
+            }
+            .buttonStyle(.bordered)
+            .controlSize(.large)
+            .tint(.teal)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Udemy/Async-Await-Actors-Concurrency/Apps/UnstructuredTasks/UnstructuredTasks/UnstructuredTasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnstructuredTasksApp.swift
+//  UnstructuredTasks
+//
+//  Created by Fabian Josue Rodriguez Alvarez on 3/4/23.
+//
+
+import SwiftUI
+
+@main
+struct UnstructuredTasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
