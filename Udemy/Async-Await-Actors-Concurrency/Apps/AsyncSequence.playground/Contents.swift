db8f59ea609a85210a28f8d9698bfbc135aaaa6f
Fabian <fabianjra@hotmail.com>
2023-04-05 13:13:42 -0600

    Udemy: Async/Await and Actors - Concurrency in Swift: -- 54. Loop Over AsyncSequence Using Await -- Se agrega el async sequence, para mostrar la informacion descargada conforme se va descargando.

--- Udemy/Async-Await-Actors-Concurrency/Apps/AsyncSequence.playground/Contents.swift
+++ Udemy/Async-Await-Actors-Concurrency/Apps/AsyncSequence.playground/Contents.swift
@@ -1,7 +1,5 @@
 import UIKit
 
-//Esta logica descarga toda la informacion, y la muestra una vez que se haya cargado completamente.
-
 extension URL {
     func allLines() async -> Lines {
         Lines(url: self)
@@ -33,8 +31,20 @@ struct LinesIterator: IteratorProtocol {
 
 let endpointURL = URL(string: "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.csv")!
 
+/*
+ //Esta logica descarga toda la informacion, y la muestra una vez que se haya cargado completamente.
 Task {
     for line in await endpointURL.allLines() {
         print(line)
+    }
+}
+*/
+
+//De esta manera, la informacion se va obteniendo y mostrando a conforme se va descargando:
+Task {
+    for try await line in endpointURL.lines {
+        print("*****************************************")
+        debugPrint(line)
+        print("*****************************************")
     }
 }
