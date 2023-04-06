4432bf4ce2960db989de78a3a254abe652a798ce
Fabian <fabianjra@hotmail.com>
2023-04-05 21:29:52 -0600

    Udemy: Async/Await and Actors - Concurrency in Swift: -- 63. Understanding Actors -- Se agrega "actor" para ejecutar una logica sobre un solo thread.

--- Udemy/Async-Await-Actors-Concurrency/Apps/foo/foo/ContentView.swift
+++ Udemy/Async-Await-Actors-Concurrency/Apps/foo/foo/ContentView.swift
@@ -7,10 +7,14 @@
 
 import SwiftUI
 
-struct Counter {
+//1: Se cambia struct por actor.
+//Actor permite que se ejcute esta clase o struct, sobre un solo Thread.
+//De esta manera, un solo thread maneja el incremento del enumarador, asi que no va a ir brincando de thread en thread para aumentar su valor, utilizando "DispatchQueue.concurrentPerform".
+actor Counter {
     var value: Int = 0
     
-    mutating func increment() -> Int {
+    //2: Se quita el mutating, porque ya no es un struct
+    /*mutating*/ func increment() -> Int {
         value += 1
         return value
     }
@@ -27,9 +31,11 @@ struct ContentView: View {
                 //Hacerlo de esta manera, ejecuta el incremetador de manera concurrente y nunca tendra un order definido.
                 DispatchQueue.concurrentPerform(iterations: 100) { _ in
                     
-                    var counter = counter
-                    
-                    debugPrint("Num incrementado: \(counter.increment())")
+                    //4: Debido a que se usa await, se debe ejecutar en Task
+                    Task {
+                        //3: Se agrega await, porque ahora es async. Esto debido a Actor
+                        debugPrint("Num incrementado: \(await counter.increment())")
+                    }
                 }
                 
             } label: {
