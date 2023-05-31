3a711ab1e88e1c1f8f0b5edfa9f7fad739afadf2
Fabian Rodriguez <fabianjra@hotmail.com>
2023-05-30 21:30:47 -0600

    Youtube: Complete SwiftUI Firebase Tutorial: Auth, Sign Up Page, Cloud Firestore, Read & Write Data. -- Link: https://www.youtube.com/watch?v=6b2WAePdiqA -- App base.

---
+++ Youtube/Firebase/FirebaseTutorial/FirebaseTutorial/FirebaseTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseTutorialApp.swift
+//  FirebaseTutorial
+//
+//  Created by Fabian Rodriguez on 30/5/23.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
