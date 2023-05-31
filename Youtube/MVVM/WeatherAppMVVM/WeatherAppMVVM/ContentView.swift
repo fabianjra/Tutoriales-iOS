8e16e6e5455952894e606701c35f3270d983275b
Fabian Rodriguez <fabianjra@hotmail.com>
2023-05-30 18:12:58 -0600

    Youtube: SwiftUI MVVM Pattern. -- 1: Link video: https://www.youtube.com/watch?v=ak8x-p-q8tU -- Base application

---
+++ Youtube/MVVM/WeatherAppMVVM/WeatherAppMVVM/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  WeatherAppMVVM
+//
+//  Created by Fabian Rodriguez on 30/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Weather App")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Youtube/MVVM/WeatherAppMVVM/WeatherAppMVVM/WeatherAppMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppMVVMApp.swift
+//  WeatherAppMVVM
+//
+//  Created by Fabian Rodriguez on 30/5/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
