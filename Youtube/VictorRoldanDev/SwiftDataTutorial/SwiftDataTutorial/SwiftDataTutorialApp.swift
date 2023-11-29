f12043fc65b83f059ede572ec3f45d5c3d3a56ab
Fabian Rodriguez <fabianjra@hotmail.com>
2023-11-29 10:48:28 -0600

    SwiftData Tutorial base project. Initial Commit

---
+++ Youtube/VictorRoldanDev/SwiftDataTutorial/SwiftDataTutorial/SwiftDataTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataTutorialApp.swift
+//  SwiftDataTutorial
+//
+//  Created by Fabian Rodriguez on 29/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Youtube/VictorRoldanDev/SwiftDataTutorial/SwiftDataTutorial/readme.md
@@ -0,0 +1,7 @@
+#  Swift Data
+
+## Pasos:
+
+1. Creacion del Model
+2. Configuracion Model Container
+3. @Query y Context en la vista.
