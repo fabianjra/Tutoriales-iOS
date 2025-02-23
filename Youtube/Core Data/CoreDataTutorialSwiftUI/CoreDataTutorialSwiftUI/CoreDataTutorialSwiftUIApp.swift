e6cbb2511ac8bf8d3d1993f9217c313cbcf4f647
Fabian Rodriguez <fabianjra@hotmail.com>
2025-02-22 22:40:43 -0600

    Se agrega la base del proyecto para el tutorial de Core Data con SwiftUI

---
+++ Youtube/Core Data/CoreDataTutorialSwiftUI/CoreDataTutorialSwiftUI/CoreDataTutorialSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataTutorialSwiftUIApp.swift
+//  CoreDataTutorialSwiftUI
+//
+//  Created by Fabian Rodriguez on 22/2/25.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataTutorialSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
